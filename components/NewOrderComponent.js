514408039f8f8dfdeee467c0c4de40a7d63aae4f
turojosephkings <turojkings@gmail.com>
2020-10-29 03:40:50 -0400

    9th Commit - New Order.choose house

--- components/NewOrderComponent.js
+++ components/NewOrderComponent.js
@@ -1,48 +1,110 @@
 import React, { Component } from 'react';
-import { Text, View, ScrollView, Picker, Switch, Button, StyleSheet } from 'react-native';
-import { Card, Icon, Rating, Input } from 'react-native-elements';
+import { Text, View, ScrollView, Picker, Switch, Button, StyleSheet, FlatList } from 'react-native';
+import { Card, Icon, Rating, Input, ListItem, Tile } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
 import DateTimePicker from '@react-native-community/datetimepicker';
+import HouseDirectory from './HouseDirectoryComponent';
+
+const mapStateToProps = state => {
+    return {
+        houses: state.houses,
+        workorders: state.workorders,
+        paymentorders: state.paymentorders
+    };
+};
+
+let id, add;
+
+function RenderHouses({houses}) {
+
+    
+    const renderHouseItem = ({item}) => {
+
+        return (
+            <ListItem
+                leftAvatar={{ source: {uri: baseUrl + item.image}}}
+                title={item.address}
+                subtitle={item.owner}
+                featured  
+                onPress={() =>
+                    this.setState({houseId: item.houseId,
+                                    address: item.address 
+                               })
+                }
+            />
+        );
+    }
+
+
+    
+    return (
+        <Card title='Select Property'>
+
+            <FlatList
+                data={houses}
+                renderItem={renderHouseItem}
+                keyExtractor={item => item.id}
+            />
+        </Card>
+    )
+}
+
+
+
 
 class NewOrder extends Component {
 
     constructor(props) {
-        super(props);
+        super(props)
 
         this.state = {
-            title: '',
+            orderType: "Maintenance Order",
+            houseId: '',
+            address: '',
+            dateReported: new Date(),
+            dateCompleted: new Date(),
             completed: false,
-            message: '',
-            date: new Date()
-        }
-    }
-
-    static navigationOptions = {
-        title: 'New Maintenance Order'
+            description: '',
+            showmodal: false
+        };
     }
 
     handleNewOrder() {
         console.log(JSON.stringify(this.state));
+       // this.toggleModal();
+    }
+
+    resetForm() {
         this.setState({
-            title: '',
+            orderType: "Maintenance Order",
+            houseId: '',
+            address: '',
+            dateReported: new Date(),
+            dateCompleted: new Date(),
             completed: false,
-            message: '',
-            date: new Date()
+            description: '',
+            showmodal: false
         });
     }
 
-        render() {
-            return (
-                <ScrollView>
-                    <View>
-                        <Text>Title of the Maintenance Order</Text>
-                            
-                        <Text>Message</Text>
-                    </View>
-                </ScrollView>
-            )
-        }
+    toggleModal() {
+        this.setState({showModal: !this.setState.showModal});
+    }
+
+
+    render() {
+
+        const { navigate } = this.props.navigation;
+        const houses = this.props.houses.houses
+        return (
+            <ScrollView>
+                <View>
+                    <RenderHouses houses={houses}/>
+                </View>
+            </ScrollView>
+        )
+    }
 
 }
 
@@ -51,4 +113,4 @@ class NewOrder extends Component {
 
 
 
-export default NewOrder;
+export default connect(mapStateToProps)(NewOrder);
