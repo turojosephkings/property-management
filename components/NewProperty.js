563e3aa198e4cd11b477d8a7f0c8cc1e5113b24a
turojosephkings <turojkings@gmail.com>
2020-11-05 19:27:29 -0500

    14th Comment - New Property Modals

--- components/NewProperty.js
+++ components/NewProperty.js
@@ -20,14 +20,38 @@ class NewProperty extends Component {
         super(props);
 
         this.state = {
-            showModal: false,
+            showRoomsModal: false,
+            showApplianceModal: false,
+            showUtilitiesModal: false,
+            showMiscellaneousModal: false,
             id: '',
             address: '',
+            imageUrl: baseUrl + './images/iconHouse.jpg',
+            sqft: '',
+            hoa: false,
+            electricprovider: '',
+            waterprovider: '',
+            fuelprovider: '',
             bedrooms: '',
             bathrooms: '',
             halfbathroom: false,
+            appliances: {
+                waterheater: false,
+                airconditioner: false,
+                furnace: false,
+                washer: false,
+                dryer: false,
+                dishwasher: false,
+                stove: false,
+                rangehood: false,
+                microwaverangehood: false,
+                refrigerator: false,
+                garagedooropener: false
+            },
+            sewertype: '',
+            petfriendly: false,
             pool: false,
-            imageUrl: baseUrl + './images/iconHouse.jpg'
+            notes: ''
         }
     }
 
@@ -36,9 +60,21 @@ class NewProperty extends Component {
     };
 
 
-    toggleModal() {
-        this.setState({showModal: !this.state.showModal});
+    toggleRoomsModal() {
+        this.setState({showRoomsModal: !this.state.showRoomsModal});
     }
+
+    toggleApplianceModal() {
+        this.setState({showApplianceModal: !this.state.showApplianceModal});
+    }
+
+    toggleUtilitiesModal() {
+        this.setState({showUtilitiesModal: !this.state.showUtilitiesModal});
+    }
+
+    toggleMiscellaneousModal() {
+        this.setState({showMiscellaneousModal: !this.state.showMiscellaneousModal});
+    }        
 
     getImageFromCamera = async () => {
         const cameraPermission = await Permissions.askAsync(Permissions.CAMERA);
@@ -58,12 +94,10 @@ class NewProperty extends Component {
 
     render() {
 
-        const { modalVisible } = this.state.showModal;        
+        const { modalVisible } = this.state.showRoomsModal;        
 
         return(
             <ScrollView>
-
-
                 <View style={styles.container}>
                     <View style={styles.imageContainer}>
                         <Image 
@@ -78,33 +112,57 @@ class NewProperty extends Component {
                     </View>
                     <Input
                         placeholder='Address'
-                        leftIcon={{type: 'font-awesome', name: 'address-card'}}
+                        leftIcon={{type: 'font-awesome', name: 'map-marker'}}
                         onChangeText={address => this.setState({address})}
                         value={this.state.address}
                         containerStyle={styles.formInput}
                         leftIconContainerStyle={styles.formIcon}
                     /> 
-                    <Button 
-                        title='Rooms'
-                        onPress={() => this.toggleModal()}
-                        buttonStyle={styles.Buttons}
-                    />  
-                                                             
-                    <CheckBox 
-                        title='Pool'
-                        center
-                        checked={this.state.pool}
-                        onPress={() => this.setState({pool: !this.state.pool})}
-                        containerStyle={styles.formCheckbox}
-                    />    
-  
+                    <Input
+                        placeholder='sqft'
+                        leftIcon={{type: 'font-awesome', name: 'map-signs'}}
+                        onChangeText={sqft => this.setState({sqft})}
+                        value={this.state.sqft}
+                        containerStyle={styles.formInput}
+                        leftIconContainerStyle={styles.formIcon}
+                    />                                                           
+                    <View style={{margin: 10}} >
+                        <Button 
+                            title='Rooms'
+                            onPress={() => this.toggleRoomsModal()}
+                            buttonStyle={styles.Buttons}
+                        />  
+                    </View>                                        
+                    <View style={{margin: 10}} >
+                        <Button 
+                            title='Appliances'
+                            onPress={() => this.toggleApplianceModal()}
+                            buttonStyle={styles.Buttons}
+                        /> 
+                      </View>     
+                      <View style={{margin: 10}} >
+                        <Button 
+                            title='Miscellaneous'
+                            onPress={() => this.toggleMiscellaneousModal()}
+                            buttonStyle={styles.Buttons}
+                        /> 
+                      </View>   
+                      <View style={{margin: 10}} >
+                        <Button 
+                            title='Utilities'
+                            onPress={() => this.toggleUtilitiesModal()}
+                            buttonStyle={styles.Buttons}
+                        /> 
+                      </View>                                                                                 
+    
+               
                 </View>
 
                 <Modal
                     animationType={'slide'}
                     transparent={false}
-                    visible={this.state.showModal}
-                    onRequestClose={() => this.toggleModal()}
+                    visible={this.state.showRoomsModal}
+                    onRequestClose={() => this.toggleRoomsModal()}
                 >
                     <SafeAreaView>
 
@@ -158,12 +216,206 @@ class NewProperty extends Component {
                             <View>
                                 <Button 
                                     title='Confirm'
-                                    onPress={() => {console.log(JSON.stringify(this.state)),this.toggleModal()}}
+                                    onPress={() => {console.log(JSON.stringify(this.state)),this.toggleRoomsModal()}}
                                 />
                             </View> 
                         </View>
                     </SafeAreaView>
-                </Modal>	                    
+                </Modal>
+
+                <Modal
+                    animationType={'slide'}
+                    transparent={false}
+                    visible={this.state.showApplianceModal}
+                    onRequestClose={() => this.toggleApplianceModal()}
+                >
+                    <SafeAreaView>
+
+                    <View style={styles.modal}>
+                    <Text style={styles.modalTitle}>Appliances Included</Text>                                             
+                    
+                        <CheckBox 
+                            title='Water Heater'
+                            center
+                            checked={this.state.waterheater}
+                            onPress={() => this.setState({waterheater: !this.state.waterheater})}
+                            containerStyle={styles.formCheckbox}
+                        />  
+                        <CheckBox 
+                            title='Air Conditioner'
+                            center
+                            checked={this.state.airconditioner}
+                            onPress={() => this.setState({airconditioner: !this.state.airconditioner})}
+                            containerStyle={styles.formCheckbox}
+                        />                          
+                        <CheckBox 
+                            title='Furnace'
+                            center
+                            checked={this.state.furnace}
+                            onPress={() => this.setState({furnace: !this.state.furnace})}
+                            containerStyle={styles.formCheckbox}
+                        /> 
+                        <CheckBox 
+                            title='Washer'
+                            center
+                            checked={this.state.washer}
+                            onPress={() => this.setState({washer: !this.state.washer})}
+                            containerStyle={styles.formCheckbox}
+                        /> 
+                        <CheckBox 
+                            title='Dryer'
+                            center
+                            checked={this.state.dryer}
+                            onPress={() => this.setState({dryer: !this.state.dryer})}
+                            containerStyle={styles.formCheckbox}
+                        />           
+                        <CheckBox 
+                            title='Dishwasher'
+                            center
+                            checked={this.state.dishwasher}
+                            onPress={() => this.setState({dishwasher: !this.state.dishwasher})}
+                            containerStyle={styles.formCheckbox}
+                        />            
+                        <CheckBox 
+                            title='Stove'
+                            center
+                            checked={this.state.stove}
+                            onPress={() => this.setState({stove: !this.state.stove})}
+                            containerStyle={styles.formCheckbox}
+                        />    
+                        <CheckBox 
+                            title='Range Hood'
+                            center
+                            checked={this.state.rangehood}
+                            onPress={() => this.setState({rangehood: !this.state.rangehood})}
+                            containerStyle={styles.formCheckbox}
+                        /> 
+                        <CheckBox 
+                            title='Microwave Range Hood'
+                            center
+                            checked={this.state.microwaverangehood}
+                            onPress={() => this.setState({microwaverangehood: !this.state.microwaverangehood})}
+                            containerStyle={styles.formCheckbox}
+                        />  
+                        <CheckBox 
+                            title='Refrigerator'
+                            center
+                            checked={this.state.refrigerator}
+                            onPress={() => this.setState({refrigerator: !this.state.refrigerator})}
+                            containerStyle={styles.formCheckbox}
+                        />         
+                        <CheckBox 
+                            title='Garage Door Opener'
+                            center
+                            checked={this.state.garagedooropener}
+                            onPress={() => this.setState({garagedooropener: !this.state.garagedooropener})}
+                            containerStyle={styles.formCheckbox}
+                        />                                                                                                                                                 
+                    </View>
+                        <View style={styles.modal}>
+                            <Text style={styles.formLabel}></Text>      
+                            <View>
+                                 
+                            </View>
+                            <View>
+                                <Button 
+                                    title='Confirm'
+                                    onPress={() => {console.log(JSON.stringify(this.state)),this.toggleApplianceModal()}}
+                                />
+                            </View> 
+                        </View>
+
+                    </SafeAreaView>
+                </Modal>	
+
+                <Modal
+                    animationType={'slide'}
+                    transparent={false}
+                    visible={this.state.showMiscellaneousModal}
+                    onRequestClose={() => this.toggleMiscellaneousModal()}
+                >
+                    <SafeAreaView>
+
+                        <View style={styles.modal}>
+                        <Text style={styles.modalTitle}>Miscellaneous Maria?</Text>                                             
+                        </View>
+
+                        <CheckBox 
+                        title='Pool'
+                        center
+                        checked={this.state.pool}
+                        onPress={() => this.setState({pool: !this.state.pool})}
+                        containerStyle={styles.formCheckbox}
+                    />    
+                    <CheckBox 
+                        title='HOA'
+                        center
+                        checked={this.state.hoa}
+                        onPress={() => this.setState({hoa: !this.state.hoa})}
+                        containerStyle={styles.formCheckbox}
+                    />                       
+                    <CheckBox 
+                        title='Pet Friendly'
+                        center
+                        checked={this.state.petfriendly}
+                        onPress={() => this.setState({petfriendly: !this.state.petfriendly})}
+                        containerStyle={styles.formCheckbox}
+                    />                          
+
+                        <View>
+                            <Button 
+                                title='Confirm'
+                                onPress={() => {console.log(JSON.stringify(this.state)),this.toggleMiscellaneousModal()}}
+                            />
+                        </View>                          
+                    </SafeAreaView>
+                </Modal>   
+
+                <Modal
+                    animationType={'slide'}
+                    transparent={false}
+                    visible={this.state.showUtilitiesModal}
+                    onRequestClose={() => this.toggleUtilitiesModal()}
+                >
+                    <SafeAreaView>
+
+                        <View style={styles.modal}>
+                        <Text style={styles.modalTitle}>Utilities Maria?</Text>                                             
+                        </View>
+
+                        <Input
+                            placeholder='Electric Provider'
+                            leftIcon={{type: 'font-awesome', name: 'plug'}}
+                            onChangeText={electricprovider => this.setState({electricprovider})}
+                            value={this.state.electricprovider}
+                            containerStyle={styles.formInput}
+                            leftIconContainerStyle={styles.formIcon}
+                        />   
+                        <Input
+                            placeholder='Water Provider'
+                            leftIcon={{type: 'font-awesome', name: 'tint'}}
+                            onChangeText={waterprovider => this.setState({waterprovider})}
+                            value={this.state.waterprovider}
+                            containerStyle={styles.formInput}
+                            leftIconContainerStyle={styles.formIcon}
+                        />          
+                        <Input
+                            placeholder='Fuel Provider'
+                            leftIcon={{type: 'font-awesome', name: 'flash'}}
+                            onChangeText={fuelprovider => this.setState({fuelprovider})}
+                            value={this.state.fuelprovider}
+                            containerStyle={styles.formInput}
+                            leftIconContainerStyle={styles.formIcon}
+                        />                          
+
+                        <View>
+                            <Button 
+                                title='Confirm'
+                                onPress={() => {console.log(JSON.stringify(this.state)),this.toggleUtilitiesModal()}}
+                            />
+                        </View>                         
+                    </SafeAreaView>
+                </Modal>                    
 
             </ScrollView>
         )
@@ -187,7 +439,7 @@ const styles = StyleSheet.create({
         marginRight: 10
     },
     formCheckbox: {
-        margin: 8,
+        margin: 7,
         backgroundColor: null
     },    
     imageContainer: {
@@ -207,19 +459,6 @@ const styles = StyleSheet.create({
     },
     formLabel: {
         fontSize: 18,
-    },
-    twoPickers: {
-        flex: 1,
-        width: 150,
-        height: 88,
-        backgroundColor: '#FFF0E0',
-        borderColor: 'black',
-        borderWidth: 1,
-        
-    },
-    twoPickerItems: {
-        height: 88,
-        color: 'red'
     },
     formButton: {
         margin: 20,
